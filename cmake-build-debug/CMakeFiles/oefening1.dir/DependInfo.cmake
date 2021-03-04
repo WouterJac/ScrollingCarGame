82750549f442695b74a197a8472ed7fc3f25d8e7
WouterJac <37212861+WouterJac@users.noreply.github.com>
2021-03-04 18:22:46 +0100

    Resources

---
+++ cmake-build-debug/CMakeFiles/oefening1.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Entities/Car.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Entities/Car.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Entities/ECar.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Entities/ECar.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Entities/EneMissile.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Entities/EneMissile.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Entities/Entity.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Entities/Entity.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Entities/Missile.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Entities/Missile.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Entities/Object.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Entities/Object.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Entities/SDL/SDLCar.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Entities/SDL/SDLCar.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Entities/SDL/SDLECar.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Entities/SDL/SDLECar.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Entities/SDL/SDLEneMissile.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Entities/SDL/SDLEneMissile.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Entities/SDL/SDLMissile.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Entities/SDL/SDLMissile.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Entities/SDL/SDLObject.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Entities/SDL/SDLObject.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Event/Event.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Event/Event.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Event/EventHandler.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Event/EventHandler.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Event/SDL/SDLEventHandler.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Event/SDL/SDLEventHandler.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Factories/AFactory.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Factories/AFactory.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Factories/SDL/SDLFactory.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Factories/SDL/SDLFactory.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Game.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Game.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/LTimer.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/LTimer.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/MenuElements/SDL/SDLbutton.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/MenuElements/SDL/SDLbutton.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/MenuElements/button.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/MenuElements/button.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Music/MusicPlayer.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Music/MusicPlayer.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/Music/SDL/SDLMusicPlayer.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/Music/SDL/SDLMusicPlayer.cpp.obj"
+  "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/main.cpp" "C:/Users/jacob/Dropbox/6 - 3de Bachelor Semester 1/6 - Geavanceerd Programmeren/Project/Sharebox/cmake-build-debug/CMakeFiles/oefening1.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/MinGW/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/oefening1.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oefening1.dir/main.cpp.obj"
+  "CMakeFiles/oefening1.dir/Factories/AFactory.cpp.obj"
+  "CMakeFiles/oefening1.dir/Factories/SDL/SDLFactory.cpp.obj"
+  "CMakeFiles/oefening1.dir/Game.cpp.obj"
+  "CMakeFiles/oefening1.dir/Entities/SDL/SDLCar.cpp.obj"
+  "CMakeFiles/oefening1.dir/Entities/Car.cpp.obj"
+  "CMakeFiles/oefening1.dir/LTimer.cpp.obj"
+  "CMakeFiles/oefening1.dir/Entities/ECar.cpp.obj"
+  "CMakeFiles/oefening1.dir/Entities/SDL/SDLECar.cpp.obj"
+  "CMakeFiles/oefening1.dir/Entities/Entity.cpp.obj"
+  "CMakeFiles/oefening1.dir/Event/EventHandler.cpp.obj"
+  "CMakeFiles/oefening1.dir/Event/SDL/SDLEventHandler.cpp.obj"
+  "CMakeFiles/oefening1.dir/MenuElements/button.cpp.obj"
+  "CMakeFiles/oefening1.dir/MenuElements/SDL/SDLbutton.cpp.obj"
+  "CMakeFiles/oefening1.dir/Music/MusicPlayer.cpp.obj"
+  "CMakeFiles/oefening1.dir/Music/SDL/SDLMusicPlayer.cpp.obj"
+  "CMakeFiles/oefening1.dir/Entities/Missile.cpp.obj"
+  "CMakeFiles/oefening1.dir/Entities/SDL/SDLMissile.cpp.obj"
+  "CMakeFiles/oefening1.dir/Entities/EneMissile.cpp.obj"
+  "CMakeFiles/oefening1.dir/Entities/SDL/SDLEneMissile.cpp.obj"
+  "CMakeFiles/oefening1.dir/Entities/Object.cpp.obj"
+  "CMakeFiles/oefening1.dir/Entities/SDL/SDLObject.cpp.obj"
+  "CMakeFiles/oefening1.dir/Event/Event.cpp.obj"
+  "oefening1.pdb"
+  "oefening1.exe"
+  "oefening1.exe.manifest"
+  "liboefening1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oefening1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
